f7bdea5d9a5c30ee0b623a30c730f827047e3560
cyent <cyent@163.com>
2017-11-08 11:53:43 +0800

    create

---
+++ docs/js/baidu-tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?98766164ed90b2faf3bbc0d33d59d858";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
